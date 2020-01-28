60609df5876dc225c79f141e15fb4fa1872832c7
zhangxinjie01 <zhangxinjie01@baidu.com>
2020-01-28 12:18:24 +0800

    Initial Commit

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by zhangxinjie01 on 2020/1/28.
+//  Copyright © 2020 ZHANG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
