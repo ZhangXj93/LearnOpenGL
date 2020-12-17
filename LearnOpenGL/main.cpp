6f1a0874f185351115992e2969ec7c477b18f2c4
zhangxinjie01 <zhangxinjie01@baidu.com>
2020-12-17 15:17:52 +0800

    摄像机 LookAt 矩阵1

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -212,7 +212,13 @@ int main()
         model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
         // 观察矩阵
         glm::mat4 view;
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+//        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        
+        float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+        
         // 投影矩阵
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), screen_width/screen_height, 0.1f, 100.0f);
